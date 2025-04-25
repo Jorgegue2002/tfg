1e74bf5042b369b897149188e2ed438ce9e1ced9
Jorge Manso Córdoba <jmansocordoba@gmail.com>
2025-04-25 18:03:25 +0200

    Hospital v1

---
+++ hospital/launch/mundo.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # Definir la ruta al archivo del mundo
+    world_path = PathJoinSubstitution([
+        get_package_share_directory('hospital'),
+        'worlds',
+        'hospital.sdf'
+    ])
+
+    # Incluir el launch oficial de ros_gz_sim para lanzar el mundo
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'world',
+            default_value=world_path,
+            description='Path to the world file'
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [get_package_share_directory('ros_gz_sim'), '/launch/gz_sim.launch.py']
+            ),
+            launch_arguments={
+                'gz_args': world_path,
+            }.items(),
+        ),
+    ])
---
+++ hospital/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+#Launch del mundo
+import os
+from glob import glob
+
+package_name = 'hospital'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+
+        #Launch del mundo
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+        
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jorge-manso-cordoba',
+    maintainer_email='jmansocordoba@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
